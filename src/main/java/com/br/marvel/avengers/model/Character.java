046111cb6ad5c242272181b49f810373d874eaaf
Jean <jeanscapim@gmail.com>
2019-10-13 21:51:53 -0300

    Added Model

--- src/main/java/com/br/marvel/avengers/model/Character.java
+++ src/main/java/com/br/marvel/avengers/model/Character.java
@@ -1,4 +1,40 @@
 package com.br.marvel.avengers.model;
 
-public class Character {
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "character", schema = "marvel")
+public class Character implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private Date createdAt;
+
+    @PrePersist
+    protected void prePersist() {
+        createdAt = new Date();
+    }
 }
