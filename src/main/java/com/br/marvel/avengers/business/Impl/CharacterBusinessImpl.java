e91701869e5c050272566dba4f1ea1a31e0f9429
Jean <jeanscapim@gmail.com>
2019-10-13 21:58:11 -0300

    Added Implement Business

--- src/main/java/com/br/marvel/avengers/business/Impl/CharacterBusinessImpl.java
+++ src/main/java/com/br/marvel/avengers/business/Impl/CharacterBusinessImpl.java
@@ -1,6 +1,40 @@
 package com.br.marvel.avengers.business.Impl;
 
 import com.br.marvel.avengers.business.CharacterBusiness;
+import com.br.marvel.avengers.model.Character;
+import com.br.marvel.avengers.repository.CharacterRepository;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class CharacterBusinessImpl implements CharacterBusiness {
+
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    @Override
+    public Optional<Character> create(@NonNull Character character) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Character> read() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Character> update(@NonNull Character character) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Character> delete(@NonNull Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Character> findById(@NonNull Integer id) {
+        return Optional.empty();
+    }
 }
